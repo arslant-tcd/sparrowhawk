c60820a84389e98b7345ce9b85b9f62a0ba300c2
Conor Church <conorchurch42@gmail.com>
2022-04-25 20:06:32 +0100

    Refactoring code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import axios from "axios";
 import '../style/Form.css'
-  
 
  // Form to display random songs, artists, genres and decades 
  // User will choose preference giving model specific data to user
@@ -32,8 +31,7 @@ import '../style/Form.css'
         }
         }).catch((error) => {
             console.log(error)
-        });
-        
+        }); 
     }
     
     // When component renders we want the suggested artists/songs to render to the screen
@@ -47,25 +45,24 @@ import '../style/Form.css'
        songInfo[id] = name
        console.log(songInfo)
        axios
-       .post("http://127.0.0.1:5000/setPreferences", {email: this.props.email, artist: this.state.selectedArtist, song: {songInfo}})
+       .put("http://127.0.0.1:5000/setPreferences", {email: this.props.email, artist: this.state.selectedArtist, song: {songInfo}})
        .then(res => {
        if(res.data["status code"] === "200"){
             this.props.parentCallback(true, this.props.email)
        }
        }).catch((error) => {
-           console.log(error)
+            console.log(error)
        });
     }
-
+    // We highlight the song selected and also change the state variable to be submitted
     handleSongClick = (song) => {
         console.log(song)
         this.setState({selectedSong: song})
-        
     }
+    // We highlight the artist selected and also change the state variable to be submitted
     handleArtistClick = (artist) => {
         console.log(artist)
         this.setState({selectedArtist: artist})
-        
     }
 
     render(){
@@ -84,7 +81,6 @@ import '../style/Form.css'
                     ))}
                 </div>
 
-
                 <div className="center-div">
                     {this.state.songs?.map((song, i) => (
                         <button key={i} className="suggestions" style={{ "backgroundColor": song === this.state.selectedSong ? "blue" : "" }} onClick={(e) => {
@@ -94,6 +90,8 @@ import '../style/Form.css'
                         </button>
                     ))}
                 </div>
+                <div> <button> Other </button></div>
+                <div> <button > Search? </button> </div>
                 <div> <button className="submitButton" onClick={this.handleSubmit(this.state.selectedSong.id, this.state.selectedSong.name)}> Submit </button> </div>
             </div>
         )
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import '../style/SignUp.css';
 import axios from "axios";
   
